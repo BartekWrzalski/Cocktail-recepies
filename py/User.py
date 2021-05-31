3dac1a9adb42cea2c3f1936998f591ebc53bee31
Bartłomiej Wrzalski (254566) <254566@student.pwr.edu.pl>
2021-05-31 20:11:57 +0200

    updated

--- py/User.py
+++ py/User.py
@@ -6,6 +6,8 @@ class LoginException(Exception): pass
 
 
 class User:
+    currentUser = None
+
     def __init__(self, login, password):
         self.__login = login
         self.__passwd = password
@@ -14,11 +16,17 @@ class User:
     def add_favourite_drink(self, cocktail):
         self.__favourite.append(cocktail)
 
+    def remove_favourite(self, coctail):
+        self.__favourite.remove(coctail)
+
     def get_login(self):
         return self.__login
 
     def get_password(self):
         return self.__passwd
+
+    def get_favourite(self):
+        return self.__favourite
 
     def __eq__(self, other):
         return self.__login == other.get_login() and self.__passwd == other.get_password()
@@ -33,31 +41,33 @@ def load_users_data():
             return []
 
 
-def save_pickled(user):
+def save_pickled():
     users = load_users_data()
     if not users:
-        users.append(user)
+        users.append(User.currentUser)
 
     for i in range(len(users)):
-        if user.__eq__(users[i]):
-            users[i] = user
+        if User.currentUser.__eq__(users[i]):
+            users[i] = User.currentUser
         if i == len(users) - 1:
-            users.append(user)
+            users.append(User.currentUser)
 
     with open(pickle_file, "wb") as file:
         pickle.dump(users, file)
+    User.currentUser = None
 
 
 def sign_in(login, password):
     users = load_users_data()
     for u in users:
         if u.get_login() == login and u.get_password() == password:
-            return u
-    raise LoginException("Incorrect login or password")
+            User.currentUser = u
+    if not User.currentUser:
+        raise LoginException("Incorrect login or password")
 
 
 def register(login, password):
     users = load_users_data()
-    if any(u.get_login() for u in users):
+    if any(u.get_login() == login for u in users):
         raise LoginException("User already exists")
-    return User(login, password)
+    User.currentUser = User(login, password)
